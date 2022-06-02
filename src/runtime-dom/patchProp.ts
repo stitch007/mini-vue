d30c0e3481b1ea8e94304e4b84ab6caed264eb58
Stitch <2273753879@qq.com>
2022-06-02 22:46:38 +0800

    fix: patchProp

--- src/runtime-dom/patchProp.ts
+++ src/runtime-dom/patchProp.ts
@@ -2,6 +2,7 @@ import { isOn } from "../shared";
 import { patchClass } from './modules/class'
 import { patchStyle } from './modules/style'
 import { patchEvent } from './modules/event'
+import { patchAttr } from './modules/attrs'
 
 // DOM属性操作
 export function patchProp(el, key, prev, next) {
@@ -10,8 +11,8 @@ export function patchProp(el, key, prev, next) {
   } else if (key === 'style') {
     patchStyle(el, prev, next)
   } else if (isOn(key)) {
-    patchClass(el, next)
-  } else {
     patchEvent(el, key, next)
+  } else {
+    patchAttr(el, key, next)
   }
 }
